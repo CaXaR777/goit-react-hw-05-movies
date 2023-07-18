71e2e8cbeb8468a291f6d859fb70048f45e21256
caxar777 <caxar7dam@gmail.com>
2023-07-18 23:55:38 +0200

    ff

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { App } from 'components/App';
+// import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -6,7 +17,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/goit-react-hw-05-movies'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
